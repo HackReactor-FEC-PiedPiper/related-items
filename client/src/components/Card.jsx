e7007309b8e59179c1196ec15e481d162edb17fc
Cameron Carruthers <cameronlcarruthers@gmail.com>
2020-08-11 12:47:36 -0600

    Add Star component to be rendered by Card component

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Stars from './Stars';
 
 const Card = ({ product }) => {
   const {
-    category, name, price, photoURL,
+    category, name, price, photoURL, ratings,
   } = product;
 
   return (
     <div className="card">
       {photoURL ? <img src={photoURL} alt={name} /> : <div className="image-placeholder" />}
-      <div className="card-content">
+      <div className="card-content red">
         <p id="category" className="light">{category}</p>
         <h3>{name}</h3>
         <p id="price" className="light">
           $
           {price}
         </p>
-        <p id="stars">#####</p>
+        <p id="stars"><Stars ratings={ratings} /></p>
       </div>
     </div>
   );
@@ -29,6 +30,13 @@ Card.propTypes = {
       name: PropTypes.string.isRequired,
       price: PropTypes.string.isRequired,
       photoURL: PropTypes.string,
+      ratings: PropTypes.shape({
+        1: PropTypes.number,
+        2: PropTypes.number,
+        3: PropTypes.number,
+        4: PropTypes.number,
+        5: PropTypes.number,
+      }),
     },
   ).isRequired,
 };
